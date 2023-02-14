6cd0c9f7ce6fcdcf6297f0e230c5b7b359cc37d0
Haruki Sakaguchi <64058773+Harusak@users.noreply.github.com>
2023-02-15 04:47:37 +0900

    add html file

---
+++ Build/Build.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:82a22207a957500cb2ed4fe562ed76182d2d6cae9823b23f9d27f765946af802
+size 516632
---
+++ Build/Build.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6129ebe009cef042a7ad9e2d8246cce1118fd6bf19d5c03af4eb06d2b4383cfc
+size 41914
